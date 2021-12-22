67a43d1b1644836988235854a21b5f708e0e1cce
thisWandiPratama <wandi99pratama#gmail.com>
2021-12-22 07:46:38 +0700

    toheroku

--- config/database-config.go
+++ config/database-config.go
@@ -5,26 +5,25 @@ import (
 	"golang_api_todo_books/entity"
 	"os"
 
-	"github.com/joho/godotenv"
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 // SetupDatabaseConnection
 func SetupDatabaseConnection() *gorm.DB {
-	errEnv := godotenv.Load()
+	// errEnv := godotenv.Load()
 
-	if errEnv != nil {
-		panic("Failed to load env file ")
-	}
+	// if errEnv != nil {
+	// 	panic("Failed to load env file ")
+	// }
 
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASS")
 	dbHost := os.Getenv("DB_HOST")
 	dbName := os.Getenv("DB_NAME")
 
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbPass, dbHost, dbName)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s sslmode=require TimeZone=Asia/Shanghai", dbUser, dbPass, dbHost, dbName)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		panic("Failed to create a connection to database ")
